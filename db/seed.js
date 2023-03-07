e723e324dcd06acd75d632ac0f7ceb7079e56140
Joel Donkoh <sethjoel.sethdonkoh@gmail.com>
2023-03-06 21:57:32 -0500

    2nd commit

--- db/seed.js
+++ db/seed.js
@@ -7,7 +7,8 @@ const {
     createPost,
     updatePost,
     getAllPosts,
-    getPostsByUser
+    getPostsByUser,
+    addTagsToPost
   } = require('./index');
   
   async function dropTables() {
@@ -105,19 +106,22 @@ const {
       await createPost({
         authorId: albert.id,
         title: "First Post",
-        content: "This is my first post. I hope I love writing blogs as much as I love writing them."
+        content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
+        tags: ["#happy", "#youcandoanything"]
       });
   
       await createPost({
         authorId: sandra.id,
         title: "How does this work?",
-        content: "Seriously, does this even do anything?"
+        content: "Seriously, does this even do anything?",
+        tags: ["#happy", "#worst-day-ever"]
       });
   
       await createPost({
         authorId: glamgal.id,
         title: "Living the Glam Life",
-        content: "Do you even? I swear that half of you are posing."
+        content: "Do you even? I swear that half of you are posing.",
+        tags: ["#happy", "#youcandoanything", "#canmandoeverything"]
       });
       console.log("Finished creating posts!");
     } catch (error) {
@@ -125,6 +129,31 @@ const {
       throw error;
     }
   }
+
+  async function createInitialTags() {
+    try {
+      console.log("Starting to create tags...");
+  
+      const [happy, sad, inspo, catman] = await createTags([
+        '#happy', 
+        '#worst-day-ever', 
+        '#youcandoanything',
+        '#catmandoeverything'
+      ]);
+  
+      const [postOne, postTwo, postThree] = await getAllPosts();
+  
+      await addTagsToPost(postOne.id, [happy, inspo]);
+      await addTagsToPost(postTwo.id, [sad, inspo]);
+      await addTagsToPost(postThree.id, [happy, catman, inspo]);
+  
+      console.log("Finished creating tags!");
+    } catch (error) {
+      console.log("Error creating tags!");
+      throw error;
+    }
+  }
+  
   
   async function rebuildDB() {
     try {
@@ -140,67 +169,10 @@ const {
     }
   }
   
-  async function testDB() {
-    try {
-      console.log("Starting to test database...");
   
-      console.log("Calling getAllUsers");
-      const users = await getAllUsers();
-      console.log("Result:", users);
-  
-      console.log("Calling updateUser on users[0]");
-      const updateUserResult = await updateUser(users[0].id, {
-        name: "Newname Sogood",
-        location: "Lesterville, KY"
-      });
-      console.log("Result:", updateUserResult);
-  
-      console.log("Calling getAllPosts");
-      const posts = await getAllPosts();
-      console.log("Result:", posts);
-  
-      console.log("Calling updatePost on posts[0]");
-      const updatePostResult = await updatePost(posts[0].id, {
-        title: "New Title",
-        content: "Updated Content"
-      });
-      console.log("Result:", updatePostResult);
-  
-      console.log("Calling getUserById with 1");
-      const albert = await getUserById(1);
-      console.log("Result:", albert);
-  
-      console.log("Finished database tests!");
-    } catch (error) {
-      console.log("Error during testDB");
-      throw error;
-    }
-  }
-  
-  async function createTags(tagList) {
-    if (tagList.length === 0) { 
-      return; 
-    }
-  
-    // need something like: $1), ($2), ($3 
-    const insertValues = tagList.map(
-      (_, index) => `$${index + 1}`).join('), (');
-    // then we can use: (${ insertValues }) in our string template
-  
-    // need something like $1, $2, $3
-    const selectValues = tagList.map(
-      (_, index) => `$${index + 1}`).join(', ');
-    // then we can use (${ selectValues }) in our string template
-  
-    try {
-      // insert the tags, doing nothing on conflict
-      // returning nothing, we'll query after
-  
-      // select all tags where the name is in our taglist
-      // return the rows from the query
-    } catch (error) {
-      throw error;
-    }
-  }
    
+  rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
   
